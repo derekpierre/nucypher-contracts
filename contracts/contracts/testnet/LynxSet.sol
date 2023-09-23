0f72500e791db5dd14b62010dcede7161041c849
derekpierre <derek.pierre@gmail.com>
2023-09-23 07:27:01 -0400

    Allow threshold staking contract to update authorization.

--- contracts/contracts/testnet/LynxSet.sol
+++ contracts/contracts/testnet/LynxSet.sol
@@ -138,10 +138,27 @@ contract LynxRootApplication is Ownable, ITACoChildToRoot {
     function withdrawRewards(address stakingProvider) external {}
 
     function authorizationIncreased(
-        address stakingProvider,
-        uint96 fromAmount,
-        uint96 toAmount // solhint-disable-next-line no-empty-blocks
-    ) external {}
+        address _stakingProvider,
+        // solhint-disable-next-line no-unused-vars
+        uint96 _fromAmount,
+        uint96 _toAmount
+    ) external {
+        require(
+            _stakingProvider != address(0) && _toAmount > 0,
+            "Input parameters must be specified"
+        );
+        require(_toAmount >= minimumAuthorization, "Authorization must be greater than minimum");
+
+        StakingProviderInfo storage info = stakingProviderInfo[_stakingProvider];
+        require(
+            _stakingProviderFromOperator[_stakingProvider] == address(0) ||
+                _stakingProviderFromOperator[_stakingProvider] == _stakingProvider,
+            "A provider can't be an operator for another provider"
+        );
+
+        info.authorized = _toAmount;
+        childApplication.updateAuthorization(_stakingProvider, _toAmount);
+    }
 
     function authorizationDecreaseRequested(
         address stakingProvider,
