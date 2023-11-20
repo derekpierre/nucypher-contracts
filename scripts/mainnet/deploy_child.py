2c8cb066cdc5dfb7b34d35b601667f134f601f3a
derekpierre <derek.pierre@gmail.com>
2023-11-20 18:36:49 -0500

    Update child deployment script to use treasury guild address constant.

--- scripts/mainnet/deploy_child.py
+++ scripts/mainnet/deploy_child.py
@@ -2,18 +2,18 @@
 
 from ape import project
 
-from deployment.constants import (
-    CONSTRUCTOR_PARAMS_DIR,
-    OZ_DEPENDENCY,
-)
+from deployment.constants import CONSTRUCTOR_PARAMS_DIR
 from deployment.params import Deployer
 
-VERIFY = False # FIXMEEEEEEE
+VERIFY = False  # FIXMEEEEEEE
 CONSTRUCTOR_PARAMS_FILEPATH = CONSTRUCTOR_PARAMS_DIR / "mainnet" / "child.yml"
+
+# Threshold Network - References:
+# - https://docs.threshold.network/resources/contract-addresses/mainnet/threshold-dao
+TREASURY_GUILD_ON_POLYGON = "0xc3Bf49eBA094AF346830dF4dbB42a07dE378EeB6"
 
 
 def main():
-
     deployer = Deployer.from_yaml(filepath=CONSTRUCTOR_PARAMS_FILEPATH, verify=VERIFY)
 
     polygon_child = deployer.deploy(project.PolygonChild)
@@ -22,22 +22,18 @@ def main():
 
     deployer.transact(polygon_child.setChildApplication, taco_child_application.address)
 
-    # TBD: proxy_admin.transferOwnership(Council) o mejor hacerlo en el constructor del proxy
     # TBD: deployer.transact(polygon_child.renounceOwnership)
 
     coordinator = deployer.deploy(project.Coordinator)
-
     deployer.transact(taco_child_application.initialize, coordinator.address)
 
-    
-
-    # Grant TREASURY_ROLE to Treasury Guild Multisig on Polygon (0xc3Bf49eBA094AF346830dF4dbB42a07dE378EeB6)
+    # Grant TREASURY_ROLE to Treasury Guild Multisig on Polygon
     TREASURY_ROLE = coordinator.TREASURY_ROLE()
-    deployer.transact(coordinator.grantRole, TREASURY_ROLE, deployer.constants.TREASURY_GUILD_ON_POLYGON)
+    deployer.transact(coordinator.grantRole, TREASURY_ROLE, TREASURY_GUILD_ON_POLYGON)
 
     # Grant INITIATOR_ROLE to Integrations Guild and BetaProgramInitiator
     INITIATOR_ROLE = coordinator.INITIATOR_ROLE()
-    deployer.transact(coordinator.grantRole, INITIATOR_ROLE, deployer.constants.TREASURY_GUILD_ON_POLYGON)  # FIXME
+    deployer.transact(coordinator.grantRole, INITIATOR_ROLE, TREASURY_GUILD_ON_POLYGON)
     # Change Coordinator admin to Council on Polygon
 
     global_allow_list = deployer.deploy(project.GlobalAllowList)
