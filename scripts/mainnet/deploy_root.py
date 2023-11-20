5749d3da607da85c3c5011b879bb5549adf7c38b
derekpierre <derek.pierre@gmail.com>
2023-11-20 18:41:43 -0500

    Update mainnet root deployment to properly deploy taco application.

--- scripts/mainnet/deploy_root.py
+++ scripts/mainnet/deploy_root.py
@@ -2,37 +2,27 @@
 
 from ape import project
 
-from deployment.constants import (
-    CONSTRUCTOR_PARAMS_DIR,
-    OZ_DEPENDENCY,
-)
+from deployment.constants import CONSTRUCTOR_PARAMS_DIR
 from deployment.params import Deployer
 
-VERIFY = True
+VERIFY = False  # TODO enable for mainnet?
 CONSTRUCTOR_PARAMS_FILEPATH = CONSTRUCTOR_PARAMS_DIR / "mainnet" / "root.yml"
+
+THRESHOLD_COUNCIL_ETH_MAINNET = "0x9F6e831c8F8939DC0C830C6e492e7cEf4f9C2F5f"
 
 
 def main():
+    deployer = Deployer.from_yaml(filepath=CONSTRUCTOR_PARAMS_FILEPATH, verify=VERIFY)
 
-    deployer = Deployer.from_yaml(
-        filepath=CONSTRUCTOR_PARAMS_FILEPATH,
-        verify=VERIFY
-    )
-
-    _ = deployer.deploy(project.TACoApplication)  # FIXME
-
-    proxy = deployer.deploy(OZ_DEPENDENCY.TransparentUpgradeableProxy)
-    taco_application = deployer.proxy(project.TACoApplication, proxy)
-
-    deployer.transact(taco_application.initialize)
+    taco_application = deployer.deploy(project.TACoApplication)
 
     polygon_root = deployer.deploy(project.PolygonRoot)
-    deployer.transact(taco_application.setChildApplication, polygon_root.address)
 
-    deployer.transact(taco_application.transferOwnership, deployer.constants["THRESHOLD_COUNCIL"])
+    # Need to set child application before transferring ownership
+    deployer.transact(taco_application.setChildApplication, polygon_root.address)
+    deployer.transact(taco_application.transferOwnership, THRESHOLD_COUNCIL_ETH_MAINNET)
 
     deployments = [
-        # proxy only (implementation has same contract name so not included)
         taco_application,
         polygon_root,
     ]
